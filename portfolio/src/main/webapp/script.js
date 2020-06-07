902c82914209a3c42dcad6c6a14d69391b3455fc
Jie <jieliangang@gmail.com>
2020-06-07 22:34:58 +0800

    Improve performance with defer and setup deployment

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,21 +13,8 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Updates job information based on selected job tab.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-
 function updateJob(index) {
     var tabs = document.getElementsByClassName("jobTab")
     for(var i = 0; i < tabs.length; i++) {
